40bfac139475b940025c7e37104cb8004a656140
Joe <hoangjd@vcu.edu>
2018-02-05 14:00:30 -0500

    Fixed UI and added variables

--- GPA_Calculator/ViewController.swift
+++ GPA_Calculator/ViewController.swift
@@ -9,11 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var titleOfCourse: UITextField!
+    @IBOutlet weak var assignmentPoint: UITextField!
+    @IBOutlet weak var midtermPoint: UITextField!
+    @IBOutlet weak var finalPoint: UITextField!
+    @IBOutlet weak var maxScoreAssignments: UITextField!
+    @IBOutlet weak var maxScoreMidterm: UITextField!
+    @IBOutlet weak var maxScoreFinal: UITextField!
+    @IBOutlet weak var percentAssignments: UITextField!
+    @IBOutlet weak var percentMidterm: UITextField!
+    @IBOutlet weak var percentFinal: UITextField!
+    @IBOutlet weak var numberOfCredits: UITextField!
+    @IBOutlet weak var deleteCourseNumber: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
